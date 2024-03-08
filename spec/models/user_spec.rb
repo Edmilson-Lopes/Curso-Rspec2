af2686d6087e60e11f7a320e0a8eb629fb8ab358
Edmilson-Lopes <milsonlopes@hotmail.com>
2024-03-07 21:25:36 -0300

    Curso Rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe User, type: :model do
     expect(User.create(nickname: 'Aizen', kind: :knight, level: 100)).to_not be_valid
  end
 
- it "returns the correct hero title"
+ it "returns the correct hero title" do 
+   user = User.create(nickname: 'Aizen', kind: :knight, level: 1)
+   expect(user.title).to eq('knight Aizen #1')
+ end
+ 
 end
