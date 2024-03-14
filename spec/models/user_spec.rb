211c0eef4f0f7a61c413c017218a807c16c6185c
Edmilson-Lopes <milsonlopes@hotmail.com>
2024-03-13 22:06:53 -0300

    Curso rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
- it "is invalid if the level is not between 1 and 99" do 
+it "is invalid if the level is not between 1 and 99" do 
    nickname = FFaker::Name.first_name
    kind = %i[knight wizard].sample 
    level = FFaker::Random.rand(100..9999)
