41bc04b7b08fec129f9c8dd974409491c9e83676
Edmilson-Lopes <milsonlopes@hotmail.com>
2024-03-29 20:52:18 -0300

    Curso Rspec

--- spec/requests/enemies_spec.rb
+++ spec/requests/enemies_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Enemies", type: :request do
-  describe "GET /enemies" do
-    it "works! (now write some real specs)" do
-      get enemies_path
-      expect(response).to have_http_status(200)
+  describe "PUT /enemies" do
+    context 'when the enemy exists' do 
+      it 'returns status code 200' do 
+        enemy = create(:enemy)
+        enemy_attributes = attributes_for(:enemy)
+        put "/enemies/#{enemy.id}", params: enemy_attributes
+        expect(response).to have_http_status(200)
+      end
+
+      it 'updates the record'
+      it 'returns the enemy updated'
+    end
+
+    context 'when the enemy does not exist' do
+      it 'return status code 404'
+      it 'return a not found message'
     end
   end
 end
